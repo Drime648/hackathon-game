02aac419290ca9462fd6bc61cf3e5a0714156b10
drime <dhruv.kandula@gmail.com>
2021-03-13 15:51:37 -0800

    change animations

--- player.gd
+++ player.gd
@@ -11,23 +11,19 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
-	var friction = false
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
-		$Sprite.play("Run")
+		$Sprite.play("walk")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
-		$Sprite.play("Run")
+		$Sprite.play("walk")
 	else:
 		$Sprite.play("Idle")
-		friction = true
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.17)
 	else: 
 		if motion.y < 0:
 			$Sprite.play("Jump")
